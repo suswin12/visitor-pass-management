14920d11d60df08e341becc3870ad2fb611ca854
suswin <suswinsuswin23@gmail.com>
2026-09-03 13:15:12 +0530

    Configure production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true,
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
